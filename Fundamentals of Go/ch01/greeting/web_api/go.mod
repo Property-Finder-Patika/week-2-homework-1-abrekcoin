a911fccd68de388c469ce399364f6e718bce7c69
abrekcoin <abrekcoin@yandex.com>
2022-07-01 14:29:12 +0300

    comm

--- Fundamentals of Go/ch01/greeting/web_api/go.mod
+++
@@ -1,7 +0,0 @@
-module main
-
-go 1.17
-
-require github.com/gorilla/mux v1.8.0
-
-replace temperature/converter => ./converter
--- Fundamentals of Go/ch04/struct/anotherFootballPlayer/footballPlayer.go
+++
@@ -1,9 +0,0 @@
-package another
-
-type FootballPlayer struct {
-	Team      string
-	Name      string
-	GoalCount int
-}
-
-var Akin FootballPlayer = FootballPlayer{Name: "Akin Kaldiroglu", Team: "Ayvalik Gücü", GoalCount: 35}
